fb34dae7d0e8cc9038330804a217122039ff6d09
Oleg Rudenko <shturzys@gmail.com>
2020-10-09 08:58:57 -0700

    Fix OSM

--- KeyboardioModel01/KeyboardioModel01.ino
+++ KeyboardioModel01/KeyboardioModel01.ino
@@ -212,14 +212,14 @@ KEYMAPS(
    Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
    Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G,
    Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
-   OSM(LeftControl), Key_Backspace, OSM(LeftGui), OSM(Key_LeftShift),
+   OSM(LeftControl), Key_Backspace, OSM(LeftGui), OSM(LeftShift),
    OSL(NUMPAD),
 
    M(MACRO_ANY),      Key_6, Key_7, Key_8,     Key_9,         Key_0,         M(MACRO_SWITCH_MAC_MODE),
-   OSM(Key_LeftGui),  Key_Y, Key_U, Key_I,     Key_O,          Key_P,         Key_Equals,
+   OSM(LeftGui),      Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
                       Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
    Key_Enter,         Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
-   OSM(Key_RightShift), OSM(Key_RightAlt), Key_Spacebar, OSM(Key_RightControl),
+   OSM(RightShift), OSM(RightAlt), Key_Spacebar, OSM(RightControl),
    OSL(FUNCTION)),
 
 #else
